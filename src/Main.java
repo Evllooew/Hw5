b09e3d90de9c9148dca0fdbcad70edc24bd16916
Evllooew <evllooev@icloud.com>
2023-06-28 11:33:22 +0300

    ДЗ исправил 2 раз

--- src/Main.java
+++ src/Main.java
@@ -171,11 +171,11 @@ public class Main {
             System.out.println(" Если возраст человека равен " + age + " годам, пора спать ");
         } else if (age > 2 && age < 7) {
             System.out.println("Если возраст человека равен " + age + " лет, нужно ходить в детский сад");
-        } else if (age > 7 && age < 18) {
+        } else if (age >= 7 && age < 18) {
             System.out.println("Если возраст человека равен " + age + " лет,нужно ходить в школу");
-        } else if (age > 18 && age < 24) {
+        } else if (age >= 18 && age < 24) {
             System.out.println("Если возраст человека равен " + age + " лет, нужно ходить в университет");
-        } else if (age > 24 && age < 60) {
+        } else if (age >= 24 && age < 60) {
             System.out.println("Если возраст человека равен " + age + " лет, пора ходить на работу");
         } else if (age >= 60) {
             System.out.println("Если возраст человека равен " + age + " лет и более,пора на пенсию");
@@ -193,7 +193,7 @@ public class Main {
 
         if (age <= 5) {
             System.out.println("Если возраст равен " + age + " нельзя кататься на аттракционе");
-        } else if (age >= 5 && age <= 14) {
+        } else if (age > 5 && age < 14) {
             System.out.println("Если возраст равен " + age + " он может кататься только в сопровождении взрослых");
         } else if (age >= 14) {
             System.out.println("Если возраст равен " + age + " он может кататься");
@@ -238,13 +238,33 @@ public class Main {
 
         if (one > two) {
 
-            if (one > three)
+            if (one > three) {
+
                 System.out.println(one);
 
-            if (two > three)
-                System.out.println(two);
+            } else {
+
+                System.out.println(three);
+
+            }
+
+        } else if (two > three) {
+
+            System.out.println(two);
+
         } else {
+
             System.out.println(three);
+
         }
+
     }
 }
+
+
+
+
+
+
+
+
